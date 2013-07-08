c8a5addee2ad5f657e4e6090d4ff6a626b9d84ad
Dinis Cruz <dinis.cruz@owasp.org>
2013-07-08 05:30:27 +0100

    Adding WMI test

---
+++ test/_test_Aspx/WMI.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Management;
+using FluentSharp.ExtensionMethods;
+
+public partial class test__test_Aspx_WMI : System.Web.UI.Page
+{
+    public string SearchQuery { get; set; }
+    public string SearchResult { get; set; }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SearchQuery = "SELECT Name, CommandLine,Caption, Description, ExecutablePath FROM Win32_Process";
+        var searcher = new ManagementObjectSearcher(SearchQuery);
+        SearchResult = searcher.Get().toList().str();
+    }
+}
